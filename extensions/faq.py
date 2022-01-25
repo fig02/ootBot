9923311e9e49181ea153a39df49107b592458cbc
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2022-01-25 14:11:27 -0500

    it comprises of -> it's comprised of
    small typo

--- extensions/faq.py
+++ extensions/faq.py
@@ -71,7 +71,7 @@ GSR - This category is very commonly recommended to beginners. It involves compl
 and doing most of Forest before beating the game. The category will teach you a lot of fundamentals that will \
 carry over to more advanced categories. A common downside cited about this category is mainly the amount of \
 cutscenes you have to sit through. There is a tutorial in the resources section.\n\n\
-No Wrong Warp (SRM) - This run is a good introduction to SRM. It comprises of doing LightNode SRM to gain control \
+No Wrong Warp (SRM) - This run is a good introduction to SRM. It\'s comprised of doing LightNode SRM to gain control \
 during the title screen to become adult with a ton of items available in the game. Within a few minutes of starting \
 the game, you can go and open the rainbow bridge and beat Ganon.\n\n\
 Glitchless - This category is great to start with because it focuses more on movement than anything else. \
